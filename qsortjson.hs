3dcfa9e46752a7ee0ad60c4b9d96bcff4b674774
Rebecca Frankel <rfrankel@alum.mit.edu>
2014-08-19 13:38:15 -0400

    redid the history structure so the lists would print at the right level. Alexey helped, particularly clueing me in to the as@ notation.

--- qsortjson.hs
+++ qsortjson.hs
@@ -7,16 +7,18 @@ import GHC.Generics
 
 main = Data.ByteString.Lazy.putStr $ encode $ quicksort ([3, 9, 2, 5, 1, 4, 8, 0, 6, 7] :: [Int])
 
-data History a = Branch [a] (History a) a [a] (History a)
+data History a = Branch [a] (History a) a (History a)
                | Leaf [a]  deriving Show
 
 instance ToJSON a => ToJSON (History a) where
-   toJSON (Branch l1 h1 p l2 h2)  = toJSON [object ["name" .=  toJSON l1, "children" .= toJSON h1], object ["name" .= toJSON p], object ["name" .=  toJSON l1, "children" .= toJSON h1]]
+   toJSON (Branch l h1 p h2)  =
+     object ["name" .= l
+            , "children" .= [toJSON h1, object ["name" .= toJSON p], toJSON h2]]
    toJSON (Leaf l) = object ["name" .= toJSON l]
 
 quicksort :: Ord a => [a] -> (History a)
 quicksort []           = Leaf []
-quicksort (pivot:tail) = Branch less (quicksort less) pivot greater (quicksort greater) 
+quicksort as@(pivot:tail) = Branch as (quicksort less) pivot (quicksort greater) 
     	      where less = [y | y <- tail, y < pivot]
                     greater = [y | y <- tail, y >= pivot]
 
