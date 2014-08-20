462f62353eda3a4a60dbc0160f0e5d74139a7d79
Rebecca Frankel <rfrankel@alum.mit.edu>
2014-08-20 13:13:16 -0400

    modified the json emitting program to also emit clock tick numbers into the json, and also wrote some functions to dump the lists in a way that would be easy to pipe to the sortvis weave program.

--- qsortjson.hs
+++ qsortjson.hs
@@ -5,21 +5,38 @@ import Data.Aeson
 import Data.ByteString.Lazy
 import GHC.Generics
 
-main = Data.ByteString.Lazy.putStr $ encode $ quicksort ([3, 9, 2, 5, 1, 4, 8, 0, 6, 7] :: [Int])
+main = Data.ByteString.Lazy.putStr $ encode $ quicksort ([3, 9, 2, 5, 1, 4, 8, 0, 6, 7] :: [Int]) 0
 
-data History a = Branch [a] (History a) a (History a)
-               | Leaf [a]  deriving Show
+type ClockTick = Int
+ 
+data History a = Branch [a] (History a) a (History a) ClockTick
+               | Leaf [a] ClockTick 
+	       deriving Show
 
 instance ToJSON a => ToJSON (History a) where
-   toJSON (Branch l h1 p h2)  =
-     object ["name" .= l
-            , "children" .= [toJSON h1, object ["name" .= toJSON p], toJSON h2]]
-   toJSON (Leaf l) = object ["name" .= toJSON ("Empty"::String)]
+   toJSON (Branch l h1 p h2 t)  =
+     object ["name" .= l,
+             "tick" .= toJSON t,
+             "children" .= [toJSON h1, object ["name" .= toJSON p, "tick" .= toJSON (t+1)], toJSON h2]]
+   toJSON (Leaf l t) = object ["name" .= toJSON ("Empty"::String), "tick" .= toJSON t]
 
-quicksort :: Ord a => [a] -> (History a)
-quicksort []           = Leaf []
-quicksort as@(pivot:tail) = Branch as (quicksort less) pivot (quicksort greater) 
+--- instance ToJSON ClockTick where
+---     toJSON t = object t
+
+quicksort :: Ord a => [a] -> ClockTick -> (History a)
+quicksort [] tck          = Leaf [] tck
+quicksort as@(pivot:tail) tck  = Branch as (quicksort less (tck+1)) pivot (quicksort greater (tck+1)) tck
     	      where less = [y | y <- tail, y < pivot]
                     greater = [y | y <- tail, y >= pivot]
 
+extractList :: Ord a => (History a) -> ClockTick -> [a]
+extractList (Branch l h1 p h2 t) tck = if t == tck then l else (extractList h1 tck) ++ [p] ++ (extractList h2 tck) 
+extractList (Leaf l t) tck = [] 
 
+highesttick :: (History a) -> ClockTick
+highesttick (Branch l h1 p h2 t) = max (highesttick h1) (highesttick h2) 
+highesttick (Leaf l t)  = t
+
+allLists :: Ord a => (History a) -> [[a]]
+allLists h = Prelude.map (extractList h) [0..n]
+    	             where n = highesttick h
